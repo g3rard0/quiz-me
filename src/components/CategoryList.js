78b50f5bfc5797617d43e0a435720b1b8a34e410
Gerardo Venegas <venegasgl@live.com>
2017-11-26 01:59:44 -0800

    Include Links to categories

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,13 +1,15 @@
 import React from 'react';
-// import categories from '../categories';
 import { Grid } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const CategoryList = ({categories}) => (
   <div className="category-list">
     <Grid padded>
         {categories.map((category) =>
           <Grid.Column key={category.value} mobile={16} tablet={8} computer={4} className="category">
-            <div className="category__type">{category.type}</div>
+            <Link to={`categories/${category.value}`}>
+              <div className="category__type">{category.type}</div>
+            </Link>
           </Grid.Column>
           )
         }
