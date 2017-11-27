6db319745a16e1086dacb4b0b7c4d9679acc58a7
Gerardo Venegas <venegasgl@live.com>
2017-11-26 23:43:02 -0800

    Refactor Diffulty component & replace buttons for links

--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
   Header,
-  Grid
+  Grid,
+  Button
 } from 'semantic-ui-react';
 import {
   Link,
@@ -14,23 +15,25 @@ class Difficulty extends Component {
     const { difficulties, match, difficultySelected} = this.props;
     return (
       <div>
-        <Header as="h3">Select difficulty</Header>
-        <div className="content">
-          <Grid padded>
-            {
-              difficulties.map(({type}) =>
-              <Grid.Column
-                key={type}
-                mobile={16}
-                tablet={8}
-                computer={4}
-                className={`difficulty selectable-item ${difficultySelected && type == difficultySelected ? 'selected': ''}`}>
-                  <div className="category__type" onClick={() => this.props.changeDifficulty(type)}>{type}</div>
+      <Header as="h3">Select difficulty</Header>
+        <Grid>
+          {
+            difficulties.map(({type}, i) =>
+            <Grid.Column
+              key={i}
+              mobile={16}
+              tablet={8}
+              computer={4}
+              className='difficulty'>
+                <Button fluid className={`selectable-item ${difficultySelected && type == difficultySelected ? 'selected': ''}`}
+                  onClick={() => this.props.changeDifficulty(type)}
+                  >
+                  {type}
+                </Button>
               </Grid.Column>
               )
             }
           </Grid>
-        </div>
       </div>
     );
   }
