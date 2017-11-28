5e475ed7daf5c3dc4a492f17971db3663e07a8c1
Gerardo Venegas <venegasgl@live.com>
2017-11-27 21:59:02 -0800

    Fetch quizzes

--- src/components/QuizList.js
+++ src/components/QuizList.js
@@ -6,16 +6,49 @@ import { parse } from 'qs';
 class QuizList extends Component {
   state = {
     isLoading: true,
-    url: 'https://opentdb.com/api.php?'
+    error: '',
+    quizList: []
   };
+  url = 'https://opentdb.com/api.php?';
 
   componentDidMount() {
     const { location } = this.props;
-    const { amount, category, difficulty, type } = parse(location.search.substr(1));
-    
+    const params = parse(location.search.substr(1));
+    this.url += Object
+      .keys(params)
+      .map(param => `${param}=${params[param]}`)
+      .join('&');
+
+    console.log(this.url);
+    axios.get(this.url)
+      .then(res => {
+        const response_code = res.data.response_code;
+        console.log(response_code);
+
+        if ([1,2].includes(response_code)) {
+          return Promise.reject(response_code);
+        }
+        console.log(res.data.results);
+        this.setState({ isLoading: false, quizList: res.data.results})
+      })
+      .catch(err_code => {
+        let message = '';
+        switch (err_code) {
+          case 1:
+            message = 'No Results';
+            break;
+          case 2:
+            message = 'Invalid Parameter';
+            break;
+          default:
+            message = 'Something went wrong. Try again later.'
+        }
+        console.log(message);
+        this.setState({ error: message, isLoading: false });
+      });
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, error, quizList } = this.state;
     if (isLoading) {
       return (
         <div>
@@ -25,9 +58,18 @@ class QuizList extends Component {
         </div>
       );
     }
+
+    if (error) {
+      return (
+        <div>
+          { error }
+        </div>
+      )
+    }
+
     return (
       <div>
-
+        <pre>{ JSON.stringify(quizList, null, 2)}</pre>
       </div>
     )
   }
