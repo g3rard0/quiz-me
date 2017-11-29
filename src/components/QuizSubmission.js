5ea7be6523d0cb8f5d9c680e246b9e54187a6d91
Gerardo Venegas <venegasgl@live.com>
2017-11-28 22:17:33 -0800

    Create quiz submission component

---
+++ src/components/QuizSubmission.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+
+const QuizSubmission = () => {
+    return (
+      <Button basic color='green'>Submit Answers</Button>
+    );
+}
+
+export default QuizSubmission;
